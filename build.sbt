f42fdce7b73ff4d4d47c4e7b1df061786ce32455
csgrdcelia <casagrande.celia@gmail.com>
2020-03-11 16:38:10 +0100

    Get COVID19 english tweets

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "twitter-hadoop"
 version := "0.1"
 
 scalaVersion := "2.12.6"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.1.0"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,43 @@
+package com.Ingeniance.spark
+import java.io.FileNotFoundException
+import java.util.Properties
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.twitter._
+
+import scala.io.Source
+
+
 object Main extends App {
-  println("Hello, World!")
+  val url = getClass.getClassLoader.getResource("application.properties")
+  val properties: Properties = new Properties()
+
+  if (url != null) {
+    val source = Source.fromURL(url)
+    properties.load(source.bufferedReader())
+  }
+
+  System.setProperty("twitter4j.oauth.consumerKey", properties.getProperty("consumerKey"))
+  System.setProperty("twitter4j.oauth.consumerSecret", properties.getProperty("consumerSecret"))
+  System.setProperty("twitter4j.oauth.accessToken", properties.getProperty("accessToken"))
+  System.setProperty("twitter4j.oauth.accessTokenSecret", properties.getProperty("accessTokenSecret"))
+
+  val ssc = new StreamingContext("local[*]", "PrintTweets", Seconds(1))
+
+  val filters = Array("COVID19")
+  val tweets = TwitterUtils.createStream(ssc, None, filters)
+
+  val statuses = tweets.map(status => status.getText())
+  statuses.print()
+
+  val englishTweets = tweets.filter(_.getLang() == "en")
+
+  englishTweets.foreachRDD{ (x, time) =>
+    if(!x.isEmpty){
+      x.saveAsTextFile("results/tweets/file" + time )
+    }
+  }
+
+  ssc.start()
+  ssc.awaitTermination()
+
 }
