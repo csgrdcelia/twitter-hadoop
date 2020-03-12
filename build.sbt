c398497c5958bff0a35efc6c4fdaaaeb0c1cb976
Vincent Guénin <guenin.vincent4@gmail.com>
2020-03-12 15:17:09 +0100

    Save tweets as one file every 5mins

--- build.sbt
+++ build.sbt
@@ -5,5 +5,11 @@ version := "0.1"
 scalaVersion := "2.11.12"
 
 libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "1.6.2"
+libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "1.6.2"
 libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "1.6.2"
 libraryDependencies += "org.apache.spark" % "spark-streaming-twitter_2.11" % "1.6.2"
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,41 +1,40 @@
-package com.Ingeniance.spark
 import java.util.Properties
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.twitter._
 
 import scala.io.Source
 
-
 object Main extends App {
-  val url = getClass.getClassLoader.getResource("application.properties")
-  val properties: Properties = new Properties()
+  override def main(args: Array[String]): Unit = {
+    val url = getClass.getClassLoader.getResource("application.properties")
+    val properties: Properties = new Properties()
 
-  if (url != null) {
-    val source = Source.fromURL(url)
-    properties.load(source.bufferedReader())
-  }
-
-  System.setProperty("twitter4j.oauth.consumerKey", properties.getProperty("consumerKey"))
-  System.setProperty("twitter4j.oauth.consumerSecret", properties.getProperty("consumerSecret"))
-  System.setProperty("twitter4j.oauth.accessToken", properties.getProperty("accessToken"))
-  System.setProperty("twitter4j.oauth.accessTokenSecret", properties.getProperty("accessTokenSecret"))
-
-  val streamingContext = new StreamingContext("local[*]", "PrintTweets", Seconds(1))
-
-  val tweets = TwitterUtils.createStream(streamingContext, None)
-
-  val statuses = tweets.map(status => status.getText())
-  statuses.print()
-
-  val englishTweets = tweets.filter(_.getLang() == "en")
-
-  englishTweets.foreachRDD{ (x, time) =>
-    if(!x.isEmpty){
-      x.saveAsTextFile("results/tweets/file" + time)
+    if (url != null) {
+      val source = Source.fromURL(url)
+      properties.load(source.bufferedReader())
     }
+
+    System.setProperty("twitter4j.oauth.consumerKey", properties.getProperty("consumerKey"))
+    System.setProperty("twitter4j.oauth.consumerSecret", properties.getProperty("consumerSecret"))
+    System.setProperty("twitter4j.oauth.accessToken", properties.getProperty("accessToken"))
+    System.setProperty("twitter4j.oauth.accessTokenSecret", properties.getProperty("accessTokenSecret"))
+
+    val streamingContext = new StreamingContext("local[*]", "PrintTweets", Minutes(5))
+
+    val tweets = TwitterUtils.createStream(streamingContext, None)
+
+    val statuses = tweets.map(status => status.getText())
+    statuses.print()
+
+    val englishTweets = tweets.filter(_.getLang() == "en")
+
+    englishTweets.foreachRDD { (x, time) =>
+      if (!x.isEmpty) {
+        x.coalesce(1).saveAsTextFile("results/tweets/file" + time)
+      }
+    }
+
+    streamingContext.start()
+    streamingContext.awaitTermination()
   }
-
-  streamingContext.start()
-  streamingContext.awaitTermination()
-
 }
